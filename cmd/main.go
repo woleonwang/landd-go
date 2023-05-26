b5112c8a99d187dbad91d79731b42afa0fee8bcb
王民艺 <115090665+woleonwang@users.noreply.github.com>
2023-05-26 13:55:03 +0800

    测试 mysql

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,45 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type HelloWorld struct {
+	ID    uint
+	Hello string
+	World string
+}
+
+func readFirstRecord(db *gorm.DB) HelloWorld {
+	var helloWorld HelloWorld
+	db.First(&helloWorld)
+	return helloWorld
+}
 
 func main() {
+	dsn := fmt.Sprintf("root:%s@tcp(localhost:3306)/landd?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("LANDD_MYSQL_PASSWORD"))
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Failed to connect database:", err)
+	}
+
+	data := readFirstRecord(db)
+
+	s, _ := json.Marshal(data)
+
 	router := gin.Default()
 
 	router.GET("/hello-world", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello World",
+			"message": s,
 		})
 	})
 
 	router.Run(":8080")
-
 }
