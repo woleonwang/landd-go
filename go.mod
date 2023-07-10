6705522c3698a021ec18129b1fa57a0ee00e0abf
王民艺 <115090665+woleonwang@users.noreply.github.com>
2023-07-10 13:03:11 +0800

    更新密码

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-contrib/requestid v0.0.6
 	github.com/gin-contrib/sessions v0.0.5
 	github.com/gin-gonic/gin v1.9.0
+	github.com/google/uuid v1.3.0
 	github.com/sirupsen/logrus v1.9.2
 	github.com/sony/sonyflake v1.1.0
 	gorm.io/driver/mysql v1.5.1
@@ -22,7 +23,6 @@ require (
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
--- pkg/mysql/db.go
+++ pkg/mysql/db.go
@@ -1,21 +1,27 @@
 package mysql
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // TODO put db config in separate config file
-const DB_USERNAME = "landd"
-const DB_PASSWORD = ""
+const DB_USERNAME = "root"
 const DB_NAME = "landd"
-const DB_HOST = ""
+const DB_HOST = "localhost"
 const DB_PORT = "3306"
 
+var DB_PASSWORD = ""
 var db *gorm.DB
 
 func Init() {
-	dsn := DB_USERNAME + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" +
+	if DB_PASSWORD == "" {
+		DB_PASSWORD = os.Getenv("LANDD_MYSQL_PASSWORD")
+	}
+
+	dsn := DB_USERNAME + ":" + DB_PASSWORD + "@(" + DB_HOST + ":" + DB_PORT + ")/" +
 		DB_NAME + "?" + "parseTime=true&loc=Local"
 
 	mysqlDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
